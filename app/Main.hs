387b5f2613751a30e88e7650d5eb0c2cfa57ceb2
Jonathan Merritt <j.s.merritt@gmail.com>
2018-01-08 20:05:44 +1100

    Initial data types

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,80 @@
 module Main where
 
-import Lib
+import qualified Data.Vector.Storable as V
+
+-- Input data
+
+data Material
+    = Material
+    { youngsModulus :: Float
+    , density       :: Float
+    , cMass         :: Float
+    , cStiffness    :: Float
+    }
+
+data Elem
+    = Elem
+    { n1       :: Int       -- first node
+    , n2       :: Int       -- second node
+    , area     :: Float     -- cross-sectional area
+    , material :: Material  -- material properties
+    }
+
+data Node
+    = Node
+    { px :: Float
+    , py :: Float
+    }
+
+data DOFDirichlet
+    = Fixed
+    | Free
+data BC
+    = BC
+    { bcNode :: Int
+    , bcx    :: DOFDirichlet
+    , bcy    :: DOFDirichlet
+    }
+
+data Force
+    = Force
+    { force :: Float -> Float  -- force as a function of time
+    }
+
+data Truss
+    = Truss
+    { nodes    :: V.Vector Node
+    , elements :: V.Vector Elem
+    }
+
+data ElemState
+    = ElemState
+    { residualStress :: Float
+    }
+
+data ElemDerived
+    = ElemDerived
+    { k11 :: Float
+    , k12 :: Float
+    , k22 :: Float
+    , c11 :: Float
+    , c12 :: Float
+    , c22 :: Float
+    }
+
+data NodeStatic
+    = NodeStatic
+    { mass :: Float
+    }
+
+data NodeState
+    = NodeState
+    { ux :: Float  -- x displacement from px
+    , uy :: Float  -- y displacement from py
+    , vx :: Float  -- x velocity
+    , vy :: Float  -- y velocity
+    }
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Hello world"
---
+++ nix-shell.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+nix-shell -p     \
+          llvm_5 \
+          clang_5
